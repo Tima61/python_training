89a6b508f6242d5064a9a843351ad1ae1af98640
51-11\timur61 <tima.bukraba@mail.ru>
2024-04-18 18:07:31 +0300

    inputs

---
+++ test_inputs.py
@@ -0,0 +1,40 @@
+# name = input("Enter your name: ")
+# print(name)
+
+# Вск данные которые мы вводим переводяться в строку!
+
+# name = input("Enter your name: ")
+# print(name)
+# print(type(name))
+#
+# age = input("Enter your age: ")
+# print(age)
+# print(type(age))
+#
+# age = int(age)
+# print(type(age))
+#
+# age = age * 2
+# print(age)
+
+# age = int(input("Enter your age: "))
+# age = age / 2
+# print(age)
+
+# amount = float(input("Процент по вкладу составит: "))
+# print(amount)
+
+shpae_leng = float(input("Enter Shape Length: "))
+shpae_width = float(input("Enter Shape Width: "))
+shpe_area = shpae_width * shpae_leng
+
+print(f"Shape area is: {shpe_area}")
+
+your_age = int(input("How old are you? "))
+
+if your_age < 18:
+    print("Your age is too young")
+else:
+    print("Your age is good")
+
+
