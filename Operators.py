482704c36c3b033370ab380235008595797844da
51-11\timur61 <tima.bukraba@mail.ru>
2024-04-18 14:58:16 +0300

    opereators

---
+++ Operators.py
@@ -0,0 +1,21 @@
+list_1 = [1, 2, 3]
+list_2 = [1, 2, 3]
+
+print(list_1 == list_2)
+# print(list_1 is list_2)
+
+##############################
+print('#'*20)
+##############################
+
+my_string_1 = 'Python'
+my_string_2 = 'Python'
+
+print(id(my_string_1), id(my_string_2))
+
+# print(my_string_1 == my_string_2)
+print(my_string_1 is my_string_2)
+
+##############################
+print('#'*20)
+##############################
