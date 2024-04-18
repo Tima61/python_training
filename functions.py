d8ea70b25c0c9b5f73dabd3bd2831b5b0700fb11
51-11\timur61 <tima.bukraba@mail.ru>
2024-04-18 10:44:45 +0300

    test commit

---
+++ functions.py
@@ -0,0 +1,24 @@
+# название функции в нижнем регистре
+# писать  комментарий в теле функции для обзначения функции она
+def say_hello():
+    """ Says hello world """
+
+    print("Hello World!")
+
+say_hello()
+
+##############################
+print('#'*20)
+##############################
+
+def say_hello_name(username):
+    """ Says hello name """
+
+    print(f"Hello {username}!")
+
+say_hello_name('Oleg')
+say_hello_name('Max')
+
+##############################
+print('#'*20)
+##############################
