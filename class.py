f0586a1423547177f2bcda8f5cc6812bb5cc2f2b
51-11\timur61 <tima.bukraba@mail.ru>
2024-04-24 17:21:59 +0300

    Наследование класса

--- class.py
+++ class.py
@@ -1,6 +1,10 @@
 # __init__ - это специальный метод в Python, который вызывается при создании экземпляра класса. Он используется для инициализации атрибутов объекта.
 # self - это обязательный первый параметр в определении каждого метода класса в Python.
 class Car:
+
+    # Атрибут класса
+    fuel_type = "Gasoline"
+
     def __init__(self, brand, model, year):
         self.brand = brand
         self.model = model
@@ -23,9 +27,28 @@ class Car:
     def increment_odometer(self, miles):
         self.odometer_reading += miles
 
+    @classmethod
+    def get_fuel_type(cls):
+        print(f"This car uses {cls.fuel_type}.")
+
+# super() - это встроенная функция Python, которая используется для вызова методов родительского класса
+class ElectricCar(Car):
+
+    # Переопределение атрибута класса
+    fuel_type = "Electricity"
+
+    def __init__(self, brand, model, year):
+        super().__init__(brand,model,year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        print(f"This car has {self.battery_size}-kWh battery.")
+
+    # Переопределение метода родительского класса
+    def read_odometer(self):
+        print(f"This electric car has {self.odometer_reading} miles on it.")
 
 # Создаем экземпляр класса Car
-
 my_car = Car('audi', 'a4', 2019)
 
 # Выводим описание автомобиля
@@ -38,3 +61,22 @@ my_car.read_odometer()
 # Увеличиваем пробег автомобиля
 my_car.increment_odometer(100)
 my_car.read_odometer()
+
+# Получаем тип топлива
+Car.get_fuel_type()
+
+# Создаем экземпляр класса ElectricCar
+my_electric_car = ElectricCar('tesla', 'model s', 2020)
+
+# Выводим описание электромобиля
+print(my_electric_car.get_descriptive_name())
+
+# Описываем батарею электромобиля
+my_electric_car.describe_battery()
+
+# Обновляем пробег электромобиля
+my_electric_car.update_odometer(100)
+my_electric_car.read_odometer()
+
+# Получаем тип топлива электромобиля
+ElectricCar.get_fuel_type()
